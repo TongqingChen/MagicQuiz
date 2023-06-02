8a133064b89dd45dd15c6e3f6c6ed0ebd0b4645d
TongqingChen <chentongqing@163.com>
2023-06-03 00:20:38 +0800

    timer precise using diff time to now

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts" setup>
 import { onBeforeUnmount, reactive, watch } from 'vue'
+import { ATime } from '@/utils/date'
 
 const data = reactive({
     timer: 0,
@@ -11,8 +12,10 @@ const data = reactive({
     text: '',
     blinkFlag: true,
     color: {},
-    is_stoped: false
+    is_stoped: false,
+    now: 0,
 })
+const time = new ATime()
 
 const props = withDefaults(defineProps<{
     start_text: string,
@@ -24,51 +27,33 @@ const props = withDefaults(defineProps<{
 
 const emit = defineEmits(['end_event'])
 
-const startTickToc = () => {
-    data.is_stoped = false
-    data.durationSecs = props.count_down ? props.duration_secs : 0
-    // 清除掉定时器
-    clearInterval(data.timer)
-    // 开启定时器
-    data.timer = window.setInterval(() => {
+const tickToc = () => {
+    data.timer = window.setTimeout(() => {
         if (props.blink) {
             data.blinkFlag = !data.blinkFlag
         }
         if (!props.blink || data.blinkFlag) {
-            let tmp = props.count_down ? (--data.durationSecs) : (++data.durationSecs)
-            const sec_min_hour_str = ['', '', '']
-            for (var i = 0; i < 3; i++) {
-                var digital = tmp % 60
-                tmp = Math.floor(tmp / 60)
-                sec_min_hour_str[i] = `00${digital}`.slice(-2)
-            }
-            data.text = `${props.start_text}${sec_min_hour_str[2]}:${sec_min_hour_str[1]}:${sec_min_hour_str[0]}`
+            var diff_sec = Math.floor((Date.now() - data.now) / 1000)
+            data.durationSecs = props.count_down ? (props.duration_secs - diff_sec) : diff_sec
+            time.setSeconds(data.durationSecs)
+            data.text = props.start_text + time.toString()
             if ((props.count_down && data.durationSecs <= 0) ||
                 (!props.count_down && data.durationSecs >= props.duration_secs)) {
                 stopTickToc()
                 emit("end_event")
+                return
             }
         }
+        tickToc()
     }, props.blink ? 500 : 1000)
 }
 const stopTickToc = () => {
     // 清除掉定时器
     if (!data.is_stoped) {
         data.is_stoped = true
-        var secs = props.count_down ? (props.duration_secs - data.durationSecs) : data.durationSecs
-        data.text = '【用时】'
-        var t = Math.floor(secs / 3600)
-        if (t > 0) {
-            data.text += `${t}时`
-        }
-        secs = secs % (3600)
-        t = Math.floor(secs / 60)
-        if (t > 0) {
-            data.text += `${t}分`
-        }
-        t = secs % 60
-        data.text += `${t}秒`
-        clearInterval(data.timer)
+        time.setSeconds(props.count_down ? (props.duration_secs - data.durationSecs) : data.durationSecs)
+        data.text = '【用时】' + time.toStringWithUnit()
+        clearTimeout(data.timer)
         data.blinkFlag = true
     }
 }
@@ -77,13 +62,19 @@ watch(
     () => props.start_flag,
     (new_v, old_v) => {
         if (new_v) {
-            startTickToc()
+            data.is_stoped = false
+            data.durationSecs = props.count_down ? props.duration_secs : 0
+            // 清除掉定时器
+            data.timer && clearTimeout(data.timer)
+            // 开启定时器
+            data.now = Date.now()
+            tickToc()
         } else {
             stopTickToc()
         }
     })
 
 onBeforeUnmount(() => {
-    clearInterval(data.timer)
+    clearTimeout(data.timer)
 })
 </script>
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -26,3 +26,27 @@ export class ADate {
         this.goToDaysLater(week_num == 0 ? -6 : (1 - week_num))
     }
 }
+
+export class ATime {
+    hours: number = 0;
+    mins: number = 0;
+    secs: number = 0;
+
+    constructor(seconds: number | null = null) {
+        if (seconds) {
+            this.setSeconds(seconds)
+        }
+    }
+    setSeconds(seconds: number) {
+        this.secs = Math.floor(seconds) % 60;
+        this.mins = Math.floor(seconds / 60) % 60
+        this.hours = Math.floor(seconds / 3600) % 60
+    }
+
+    toString() {
+        return `0${this.hours}:`.slice(-3) + `0${this.mins}:`.slice(-3) + `0${this.secs}`.slice(-2)
+    }
+    toStringWithUnit() {
+        return `0${this.hours}时`.slice(-3) + `0${this.mins}分`.slice(-3) + `0${this.secs}秒`.slice(-3)
+    }
+}
